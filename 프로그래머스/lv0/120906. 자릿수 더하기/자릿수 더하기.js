f7bf1db8e206c52774a8581a787a84c99153bc40
Song Yunbi <yunbiyomi@naver.com>
2023-07-09 00:02:16 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,10 +1,10 @@
 function solution(n) {
     let hap = 0;
     
-    while(n>9){
+    while(n>0){
         hap += n%10;
         n = Math.floor(n/10);
     }
     
-    return hap+n;
+    return hap;
 }
