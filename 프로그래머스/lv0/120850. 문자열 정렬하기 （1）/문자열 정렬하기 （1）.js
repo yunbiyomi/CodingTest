3e1eb1e1950152db6ca91b02969a5d9845564c12
Song Yunbi <yunbiyomi@naver.com>
2023-07-10 01:11:39 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-    return my_string.replace(/[a-z]/gi,"").split('').map(Number).sort((a, b)=>a-b);
+    return my_string.replace(/[a-z]/gi,"").split('').map(Number).sort();
 }
