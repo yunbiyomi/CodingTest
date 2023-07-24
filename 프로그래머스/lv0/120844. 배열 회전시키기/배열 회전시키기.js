5742ae305f6f1d4779739cf1d666ee800cf366f1
Song Yunbi <yunbiyomi@naver.com>
2023-07-25 01:01:00 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,19 +1,8 @@
 function solution(numbers, direction) {
-    if (direction === "right") {
-        const num = numbers[numbers.length-1];
-        for(let i=numbers.length-1; i>0; i--) {
-            numbers[i] = numbers[i-1];
-        }
-        numbers[0] = num;
-    }
-    
-    else {
-        const num = numbers[0];
-        for(let i=0; i<numbers.length; i++) {
-            numbers[i] = numbers[i+1];
-        }
-        numbers[numbers.length-1] = num;
-    }
+    if(direction === "right")
+        numbers.unshift(numbers.pop());
+    else
+        numbers.push(numbers.shift());
     
     return numbers;
 }
