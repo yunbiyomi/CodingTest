4a8ee5addc18c554c87a41c7c94c63ceb99c4782
Song Yunbi <yunbiyomi@naver.com>
2023-07-24 23:26:40 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,11 +1,7 @@
 function solution(age) {
-    const ageToNumber = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7, i: 8, j: 9 };
-    
-    const newAge = age.toString().split('');
-    
-    const ageDigits = newAge.map(digit => {
-        return Object.keys(ageToNumber).find(key => ageToNumber[key] === Number(digit));
-    });
-    
-    return ageDigits.join('');
+   return age
+    .toString()
+    .split('')
+    .map((v) => "abcdefghij"[v])
+    .join('');
 }
