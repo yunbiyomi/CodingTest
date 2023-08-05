192eb390d2465fcd2ef2ae3750981629316b18f4
Song Yunbi <yunbiyomi@naver.com>
2023-08-06 00:15:23 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,18 +1,5 @@
 function solution(id_pw, db) {
-    let answer = '';
-    
-    for(let i in db){
-        if(JSON.stringify(id_pw[0])===JSON.stringify(db[i][0])){
-           if(JSON.stringify(id_pw)===JSON.stringify(db[i]))   
-               answer = 'login';
-            else if(JSON.stringify(id_pw[0])===JSON.stringify(db[i][0]) && JSON.stringify(id_pw[1])!==JSON.stringify(db[i][1]))
-                answer = 'wrong pw'
-            break;
-        }
-    
-        else
-            answer = 'fail';
-    }
-    
-    return answer
+  const [id, pw] = id_pw;
+  const map = new Map(db);
+  return map.has(id) ? (map.get(id) === pw ? 'login' : 'wrong pw') : 'fail';
 }
