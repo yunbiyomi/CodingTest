7b27777a934795426daf1de7c33cf3229116d3f6
Song Yunbi <yunbiyomi@naver.com>
2023-08-05 23:01:45 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,15 +1,3 @@
 function solution(my_string) {
-    let hap = 0;
-    const numArray = my_string.match(/\d+/g);
-    
-    if(numArray===null)
-        return 0;
-    else{
-        const hapArray = numArray.map(Number);
-        
-        for(let i of hapArray)
-            hap+=i;
-
-        return hap;
-    }
+    return my_string.split(/\D+/).reduce((acc, cur) => acc + Number(cur), 0);
 }
