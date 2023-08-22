6d86b086b5d7fa177b4737c9a4324d787a55b473
Song Yunbi <yunbiyomi@naver.com>
2023-08-22 23:17:54 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,14 @@
+function solution(s){
+    let strArr = s.toUpperCase().split('');
+    let pHap = 0;
+    let yHap = 0;
+    
+    for(let i of strArr){
+        if(i==='P')
+            pHap++;
+        if(i==='Y')
+            yHap++;
+    }
+
+    return pHap === yHap ? true : false;
+}
