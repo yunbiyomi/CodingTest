8ee847e5d3243c7b1f06597805b37f1460226d77
Song Yunbi <yunbiyomi@naver.com>
2023-09-07 22:12:27 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,16 @@
+function solution(left, right) {
+    var answer = 0;
+    
+    for(let i=left; i<=right; i++){
+        let count = 0;
+        
+        for(let j=1; j<=i; j++){
+            if(i%j===0)
+                count++;
+        }
+        
+        answer += count%2 ? -i : +i;
+    }
+    
+    return answer;
+}
