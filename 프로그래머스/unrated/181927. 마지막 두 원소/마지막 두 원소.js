51ad1544896e858372789efb126a34831b6c6a2e
Song Yunbi <yunbiyomi@naver.com>
2023-10-08 18:45:39 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,5 +1,7 @@
 function solution(num_list) {
-    num_list[num_list.length-1] > num_list[num_list.length-2] ? num_list.push(num_list[num_list.length-1]-num_list[num_list.length-2]) : num_list.push(num_list[num_list.length-1]*2);
+    num_list[num_list.length-1] > num_list[num_list.length-2] 
+        ? num_list.push(num_list[num_list.length-1]-num_list[num_list.length-2]) 
+        : num_list.push(num_list[num_list.length-1]*2);
     
     return num_list;
 }
