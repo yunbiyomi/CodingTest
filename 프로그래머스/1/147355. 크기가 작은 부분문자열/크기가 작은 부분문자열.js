f10afd4059b0ac37a443747cdb5a4d1832e3813c
Song Yunbi <yunbiyomi@naver.com>
2023-10-12 23:24:44 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,20 +1,8 @@
 function solution(t, p) {
-    const length = p.split("").length;
-    const tSplit = t.split("");
-    let tArr = [];
     let count = 0;
-    
-    for(let i=0; i<tSplit.length-length+1; i++){
-        let num = 0;
-        for(let j=0; j<length; j++)
-            num+=tSplit[i+j];
-        tArr.push(Number(num));
+    for(let i=0; i<=t.length-p.length; i++) {
+        let value = t.slice(i, i+p.length);
+        if(+p >= +value) count++;
     }
-    
-    for(let i of tArr){
-        if(i<=p)
-            count++;
-    }
-    
     return count;
 }
