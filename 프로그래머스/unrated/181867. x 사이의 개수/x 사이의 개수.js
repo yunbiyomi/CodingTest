0eb761c4cef5ab6cd39fb5e978afdf155eef2247
Song Yunbi <yunbiyomi@naver.com>
2023-10-16 22:55:40 +0900

    [level unrated] Title: x 사이의 개수, Time: 1.33 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.js
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.js
@@ -1,9 +1,3 @@
 function solution(myString) {
-    const standard = myString.split("x")
-    let answer = [];
-    
-    for(let i of standard)
-        answer.push(i.length);
-    
-    return answer;
+    return myString.split("x").map(i => i.length);
 }
