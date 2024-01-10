790dde80b44d12bc3a60b0a019ae408c220a3ece
Song Yunbi <yunbiyomi@naver.com>
2024-01-10 22:26:52 +0900

    [level 1] Title: 소수 만들기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,15 @@
+function solution(nums) {
+    let answer = 0, number=0;
+    for(var i=0;i<nums.length-2;i++){
+        for(var j=i+1;j<nums.length-1;j++){
+            for(var l=j+1;l<nums.length;l++){
+                number=nums[i]+nums[j]+nums[l];
+                let count=0;
+                for(var k=1;k<=number;k++)
+                    if(number%k==0) count++;
+                if(count==2) answer++;
+            }
+        }
+    }
+    return answer;
+}
